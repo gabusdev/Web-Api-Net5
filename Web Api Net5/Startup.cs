3ae622f1f583b95c1d284c08eacd1993a092e0c0
gabus <gabusdev@outlook.com>
2022-01-23 15:58:22 -0500

    Added Full Permisive Cors Policy and Being used

--- Web Api Net5/Startup.cs
+++ Web Api Net5/Startup.cs
@@ -26,7 +26,14 @@ namespace Web_Api_Net5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                 );
+            });
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -45,6 +52,8 @@ namespace Web_Api_Net5
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
